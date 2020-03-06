9faae439ecf64205ccf713eeefa8cb15ff7d5e60
Brown <brownw4@egb080000001.lancs.local>
2020-03-06 16:56:07 +0000

    le epic

---
+++ Driver.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Driver{
+    public static void main (String[] args){
+        Board board = new Board(1000,1000,"le title lmao");
+        board.createScreen();
+    }
+}
