1e311809560d836fd54284fda4bf7ff06399eaf0
laika54 <60370997+laika54@users.noreply.github.com>
2020-03-17 23:32:27 +0000

    main game finished
    requires more testing, level select, and new game when game finished

--- Board.java
+++ Board.java
@@ -18,7 +18,8 @@ public class Board implements ActionListener{
     private File file;
     private BufferedReader br;
     Square[] squares = new Square[25];
-    Square testSquare = new Square(0,0);
+    private int[] selected = {-1,-1};
+    //Square testSquare = new Square(0,0);
     
 
     
@@ -60,7 +61,6 @@ public class Board implements ActionListener{
         }
         for (int i = 0; i<5; i++){
             for (int j = 0; j<5; j++){
-                squares[(i*5)+j] = new Square(i,j);
                 
                 try{
                     sqr = br.readLine();
@@ -72,25 +72,93 @@ public class Board implements ActionListener{
 
                 switch(sqr){
                     case "r":
+                        squares[(i*5)+j] = new Square(j,i,"RedFrog");
                         squares[(i*5)+j].setImage("RedFrog");
                         break;
                     case "g":
+                        squares[(i*5)+j] = new Square(j,i,"GreenFrog");
                         squares[(i*5)+j].setImage("GreenFrog");
                         break;
                     case "w":
+                        squares[(i*5)+j] = new Square(j,i,"Water");
                         squares[(i*5)+j].setImage("Water");
                         break;
                     case "l":
+                        squares[(i*5)+j] = new Square(j,i,"LilyPad");
                         squares[(i*5)+j].setImage("LilyPad");
                         break;
                 }
 
                 panel.add(squares[(i*5)+j].button, GLayout);
+                squares[(i*5)+j].button.addActionListener(this);
             }
         }
     }
 
-    public void actionPerformed(ActionEvent e){
+    public int[] findCentre(Square sq1, Square sq2){
+        int[] coords = {-1,-1};
+        try{
+            coords[0] = (sq1.getx()+sq2.getx())/2;
+            coords[1] = (sq1.gety()+sq2.gety())/2;
+            return coords;
+        }catch(Exception e){
+            System.out.println("not valid centre");
+        }
+        return coords;
+    }
 
+    public int getIndex(int xcord, int ycord){
+        return (ycord*5)+xcord;
+    }
+
+    public boolean winner(){
+        for (int i = 0; i < 25; i++){
+            if(squares[i].getPiece().equals("GreenFrog")){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void actionPerformed(ActionEvent e){
+        //System.out.println(e.getSource());
+        
+        for (int i = 0; i<25; i++){
+            if (e.getSource() == squares[i].button){
+                System.out.println(squares[i].getx() + " " + squares[i].gety());
+                if(squares[i].getPiece().equals("Water") == false){
+                    if (selected[0] == -1){
+                        if(squares[i].getPiece().equals("LilyPad") == false){
+                            selected[0] = i;
+                            squares[i].setImage(squares[i].getPiece()+"2");
+                        }
+                    }else{
+                        selected[1]=i;
+                        int[] centre = {-1,-1};
+                        centre = findCentre(squares[selected[0]], squares[selected[1]]);
+                        //int centreIndex =
+                        if (squares[selected[0]].checkLegal(squares[selected[1]], squares[getIndex(centre[0],centre[1])], centre)){
+                            squares[selected[0]].moveSquare(squares[selected[1]]);
+
+                            squares[getIndex(centre[0],centre[1])].setPiece("LilyPad");
+                            squares[getIndex(centre[0],centre[1])].setImage("LilyPad");
+                            
+                            selected[0]=-1;
+                            selected[1]=-1;
+                        }else{
+                            System.out.println("move illegal");
+                            System.out.println(squares[selected[1]].getPiece());
+                        }
+                        //selected[0]=-1;
+                        selected[1]=-1;
+                    }
+                }
+            }
+        }
+
+        if (winner()){
+            JOptionPane.showMessageDialog(null, "CATASTROPHIC ERROR:\nyou win :)");
+        }
     }
 }
--- Square.java
+++ Square.java
@@ -1,23 +1,25 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.lang.*;
 
 public class Square{
 
-    public JButton button;
+    public JButton button = new JButton();
     private int x;
     private int y;
-    private ImageIcon i;
+    private Icon i;
     private String piece;
 
-    public Square(int xcord, int ycord){
+    public Square(int xcord, int ycord, String p){
         x = xcord;
         y = ycord;
+        piece = p;
         
     }
 
     public void setImage(String img){
         i = new ImageIcon(img+".png");
-        button = new JButton(i);
+        //button.JButton(i);
         button.setIcon(i);
     }
 
@@ -28,6 +30,14 @@ public class Square{
     public int gety(){
         return y;
     }
+    
+    public void setx(int num){
+        x = num;
+    }
+
+    public void sety(int num){
+        y = num;
+    }
 
     public void setPiece(String p){
         piece = p;
@@ -37,11 +47,50 @@ public class Square{
         return piece;
     }
 
-    public boolean checkLegal(Square square){
+    public boolean checkLegal(Square squareMoveTo, Square centreSquare, int[] centre){
         //check if move is legal
-        //check if THIS SQUARE -- IE THE INSTANCE  THIS COMMENT IS IN RIGHT NOW is a frog
-        //check is second piece is a lily
-        //check if lily is close enough
-        //check if jumping over a frog -- _check if square between two squares is a frog_
+            //check is second piece is a lily
+        int changey = y - squareMoveTo.gety();
+        int changex = x - squareMoveTo.getx();
+        if (squareMoveTo.getPiece().equals("LilyPad") == false){
+            System.out.println("Piece to land on was not a lilypad");
+            return false;
+
+        }
+            //check if lily is close enough ----- total change x and y must be 4
+        else if (Math.abs(changex)+Math.abs(changey) != 4){
+            System.out.println("Distance not valid");
+            System.out.println(changex + "piss");
+            System.out.println((x+y) + "\t" + squareMoveTo.getx() + " " + squareMoveTo.gety() + "piss");
+            return false;
+        }
+            
+            
+            //check if jumping over a frog -- _check if square between two squares is a frog_
+        else if (centreSquare.getPiece().equals("GreenFrog") == false){
+            System.out.println("Centre was not a frog");
+            return false;
+        }else if (centre[0] == -1){
+            System.out.println("Centre invalid");
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public void moveSquare(Square square){
+        //getIndex(x,y);
+        square.setImage(piece);
+        square.setPiece(piece);
+
+        setImage("LilyPad");
+        setPiece("LilyPad");
+
+        //setx(square.getx());
+        //sety(square.gety());
+    }
+
+    public void testMethod(Square square){
+        System.out.println(square.getx() + square.getPiece());
     }
 }
